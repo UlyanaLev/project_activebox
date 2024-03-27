9c04dc945433ad3bb9fede71bace954269a46ff6
Ulyana Levchuk <ulyana.liauchuk@gmail.com>
2024-03-27 22:23:20 +0300

    Add work and our_team

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,14 @@
 console.log("Alloha! Bro");
+
+window.addEventListener('scroll', () => {
+    var scrollDistance = window.scrollY;
+    var headerStyle = document.querySelector('.header');
+
+    if (scrollDistance >= 1) {
+        headerStyle.classList.add('header-scroll');
+    } else {
+        headerStyle.classList.remove('header-scroll');
+    }
+
+    // console.log('scroll distance: ' + scrollDistance);
+});
