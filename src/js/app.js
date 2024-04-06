8f0d3304790bd8837277e591fd543114a292b2ab
Ulyana Levchuk <ulyana.liauchuk@gmail.com>
2024-04-06 16:15:15 +0300

    Fixed

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 console.log("Alloha! Bro");
 
+//header
 window.addEventListener('scroll', () => {
     var scrollDistance = window.scrollY;
     var headerStyle = document.querySelector('.header');
@@ -13,28 +14,41 @@ window.addEventListener('scroll', () => {
     // console.log('scroll distance: ' + scrollDistance);
 });
 
+//burger
 document.querySelector('.burger').addEventListener('click', () => {
     var headerWrap = document.querySelector('.header-wrap');
     var burgerBtn = document.querySelector('.burger');
+    var bodyScroll = document.querySelector('body');
     
     headerWrap.classList.toggle('menu-wrap');
     burgerBtn.classList.toggle('active');
+    bodyScroll.classList.toggle('body_hidden');
 
     document.querySelector('.logo').addEventListener('click', () => {
         headerWrap.classList.remove('menu-wrap');
         burgerBtn.classList.remove('active');
+        bodyScroll.classList.remove('body_hidden');
     });
 
     document.querySelector('.navigation-list').addEventListener('click', () => {
         headerWrap.classList.remove('menu-wrap');
         burgerBtn.classList.remove('active');
+        bodyScroll.classList.remove('body_hidden');
     });
 });
 
+//swiper-slides
 var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
     spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
+    mousewheel: true,
+    keyboard: true,
   });
